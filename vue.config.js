74d024705a13b18b142e4a135186dbf80814feac
akihiro-kondo <2107sum@gmail.com>
2025-01-25 11:25:46 +0900

    deploy設定修正

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: '/practice-Vue-app/', // リポジトリ名を設定
+};
+
+
+
